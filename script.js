61b98414bc9f64843c44acf1a0b493aca3110118
trentmoss05 <trentmoss05@gmail.com>
2019-06-29 01:08:19 -0400

    added for loop for adding initial colors and picking color

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-var squares = document.getElementsByClassName("square");
+var squares = document.querySelectorAll(".square");
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
+];
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for (var i = 0; i < squares.length; i++) {
+  //add initial colors
+  squares[i].style.backgroundColor = colors[i];
+  //add click listeners to squares
+  squares[i].addEventListener("click", function() {
+    //grab color of clicked on square and compare to pickedColor
+    var clickedColor = this.style.backgroundColor;
+    if (clickedColor === pickedColor) {
+      alert("correct");
+    } else {
+      this.style.backgroundColor = "#232323";
+    }
+  });
+}
